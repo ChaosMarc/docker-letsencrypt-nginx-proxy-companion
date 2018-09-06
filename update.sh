c952f551a8494f94af97b342168af39c9afb69c2
ChaosMarc <chaosmarc@gmx.de>
2018-09-06 15:03:22 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ cd "${0%/*}"
 git remote add upstream https://github.com/JrCs/docker-letsencrypt-nginx-proxy-companion.git
 git fetch upstream
 git pull -X theirs upstream master
-sed -i -e 's/amd64/armhf/g' Dockerfile
 if [[ "$(head -1 README.md)" != armhf* ]]; then
 	echo -e 'armhf port of [jrcs/letsencrypt-nginx-proxy-companion](https://github.com/JrCs/docker-letsencrypt-nginx-proxy-companion): [keksnase/letsencrypt-nginx-proxy-companion-armhf](https://hub.docker.com/r/keksnase/letsencrypt-nginx-proxy-companion-armhf/)\n' | cat - README.md > temp && mv temp README.md
 fi
